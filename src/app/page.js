2258cea88206d190f409868baf539f06381954bf
Ashik-Himel <ashikujjamanhimel@gmail.com>
2024-10-27 15:50:16 +0600

    Section Design Completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,65 @@
 import Image from "next/image";
+import { FaArrowRightLong } from "react-icons/fa6";
+import line from "@/assets/line.png";
+import building from "@/assets/building.png";
+import house from "@/assets/house-icon.png";
+import sectionImg from "@/assets/section-image.png";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main>
+      <section className="bg-gray-100 py-16">
+        <div className="container flex flex-col lg:flex-row justify-center lg:items-center gap-8">
+          <div className="flex-1">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={line}
+              alt="Line Vector"
+              className="hidden lg:block w-[120px] relative -left-[30px] -top-[30px]"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <span className="bg-white px-4 py-2 rounded-full inline-flex justify-center items-center gap-2 mb-4">
+              <FaArrowRightLong />
+              About Us
+            </span>
+            <h2 className="text-4xl font-bold leading-[1.2] mb-6">We design thoughtful, liveable spaces</h2>
+            <p className="mb-4">The barn house captivates with its blend of rustic charm and modern sophistication, featuring weathered wood, sleek metal accents.</p>
+            <div className="flex flex-col sm:flex-row lg:flex-col gap-4">
+              <div className="bg-white p-4 rounded-lg flex justify-center items-center gap-4">
+                <div>
+                  <Image
+                    src={building}
+                    alt="Building Icon"
+                    className="w-[120px]"
+                  />
+                </div>
+                <div>
+                  <h4 className="text-xl font-semibold mb-2">Commercial</h4>
+                  <p>The barn house captivates with its blend of rustic charm and modern.</p>
+                </div>
+              </div>
+              <div className="bg-white p-4 rounded-lg flex justify-center items-center gap-4">
+                <div>
+                  <Image
+                    src={house}
+                    alt="House Icon"
+                    className="w-[120px]"
+                  />
+                </div>
+                <div>
+                  <h4 className="text-xl font-semibold mb-2">Residential</h4>
+                  <p>The barn house captivates with its blend of rustic charm and modern.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex-1">
+            <Image
+              src={sectionImg}
+              alt="Section Image"
+              className="w-full aspect-video lg:aspect-auto xl:max-w-[500px] xl:mx-auto"
+            />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </main>
   );
 }
